16326bf444d8fe1ae7cb98607a4dc6681a51bcd6
paraglande03 <paraglande03@gmail.com>
2021-03-04 00:06:22 +0530

    Added: test to check peak of queue

---
+++ src/main/java/MyQueue.java
@@ -0,0 +1,16 @@
+public class MyQueue {
+    private final MyLinkedList myLinkedList;
+    public MyQueue() {
+        this.myLinkedList = new MyLinkedList();
+    }
+    public void queue(INode myNode) {
+        myLinkedList.add(myNode);
+    }
+    public void printQueue() {
+        myLinkedList.printMyNodes();
+    }
+    public INode peak() {
+        return myLinkedList.head;
+    }
+
+}
---
+++ src/test/java/MyQueueTest.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyQueueTest {
+    MyQueue myQueue=new MyQueue();
+    @Test
+    public void givenThreeNoWhenAddedToQueueShouldReturnTrue() {
+        MyNode<Integer> myFirstNode=new MyNode<>(56);
+        MyNode<Integer> mySecondNode=new MyNode<>(30);
+        MyNode<Integer> myThirdNode=new MyNode<>(70);
+        myQueue.queue(myFirstNode);
+        myQueue.printQueue();
+        myQueue.queue(mySecondNode);
+        myQueue.printQueue();
+        myQueue.queue(myThirdNode);
+        myQueue.printQueue();
+        INode peak=myQueue.peak();
+        Assert.assertEquals(peak, myThirdNode);
+    }
+}
